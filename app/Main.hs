bb553289b6fecfcd6878d03e01dcde14b4430911
Adam <adamweider98@gmail.com>
2020-09-06 18:50:08 -0500

    Run ormolu on files.

--- app/Main.hs
+++ app/Main.hs
@@ -1,32 +1,36 @@
-{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+
 module Main (main) where
 
 import Import
-import Run
-import RIO.Process
 import Options.Applicative.Simple
 import qualified Paths_scrabbelm_server
+import RIO.Process
+import Run
 
 main :: IO ()
 main = do
-  (options, ()) <- simpleOptions
-    $(simpleVersion Paths_scrabbelm_server.version)
-    "Header for command line arguments"
-    "Program description, also for command line arguments"
-    (Options
-       <$> switch ( long "verbose"
-                 <> short 'v'
-                 <> help "Verbose output?"
-                  )
-    )
-    empty
+  (options, ()) <-
+    simpleOptions
+      $(simpleVersion Paths_scrabbelm_server.version)
+      "Header for command line arguments"
+      "Program description, also for command line arguments"
+      ( Options
+          <$> switch
+            ( long "verbose"
+                <> short 'v'
+                <> help "Verbose output?"
+            )
+      )
+      empty
   lo <- logOptionsHandle stderr (optionsVerbose options)
   pc <- mkDefaultProcessContext
   withLogFunc lo $ \lf ->
-    let app = App
-          { appLogFunc = lf
-          , appProcessContext = pc
-          , appOptions = options
-          }
+    let app =
+          App
+            { appLogFunc = lf,
+              appProcessContext = pc,
+              appOptions = options
+            }
      in runRIO app run
