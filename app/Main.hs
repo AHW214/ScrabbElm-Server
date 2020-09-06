d830d5c7afad6505139541031af9d773a2a509c2
Adam <adamweider98@gmail.com>
2020-09-06 17:54:55 -0500

    Init rio project template.

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,32 @@
-module Main where
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE TemplateHaskell #-}
+module Main (main) where
 
---------------------------------------------------------------------------------
+import Import
+import Run
+import RIO.Process
+import Options.Applicative.Simple
+import qualified Paths_scrabbelm_server
+
 main :: IO ()
-main = putStrLn "time to redo many things"
+main = do
+  (options, ()) <- simpleOptions
+    $(simpleVersion Paths_scrabbelm_server.version)
+    "Header for command line arguments"
+    "Program description, also for command line arguments"
+    (Options
+       <$> switch ( long "verbose"
+                 <> short 'v'
+                 <> help "Verbose output?"
+                  )
+    )
+    empty
+  lo <- logOptionsHandle stderr (optionsVerbose options)
+  pc <- mkDefaultProcessContext
+  withLogFunc lo $ \lf ->
+    let app = App
+          { appLogFunc = lf
+          , appProcessContext = pc
+          , appOptions = options
+          }
+     in runRIO app run
